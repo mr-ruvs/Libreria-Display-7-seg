492efedeff6b121bd576cc1b931e4e7fdbd15f7a
Rubén David Granados Amorín <107018164+mr-ruvs@users.noreply.github.com>
2022-06-08 22:01:47 -0600

    Add files via upload

---
+++ Examples/Demostracion_Funciones/Demostracion_Funciones.ino
@@ -0,0 +1,17 @@
+#include <display7seg.h>
+byte pines[] = {6, 11, 9, 8, 7, 5, 3, 10}; // segmentos a, b, c, d, e, f, g, dp
+// pines seleccionados
+// 0 para CATODO comun --- 1 para ANODO comun
+// 0 para inhabilitar el dp --- 1 para habilitar el dp
+Display7s d1(pines, 0, 1);                 // objeto tipo Display7s; se declaran los pines, 0 para catodo, 1 para activar dp
+void setup() {
+
+}
+
+void loop() {
+  d1.prueba(500);                           // enciende segmento por segmento y luego apaga segmento por segmento cada 500 ms
+  for (int i = 0; i <= 15; i++) {          // recorre los numeros del 0 al 15
+    d1.mostrar(i);                         // muestra los numeros hexadecimal
+    delay(500);                            // una espera de 500 ms
+  }
+}
